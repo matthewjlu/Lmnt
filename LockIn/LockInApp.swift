63811b13b6eebd7d34c861edafb08c7930abe99a
matthewjlu <mlu072005@gmail.com>
2025-05-08 23:30:12 -0700

    Initial Commit

---
+++ LockIn/LockInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockInApp.swift
+//  LockIn
+//
+//  Created by Matthew Lu on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LockInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
